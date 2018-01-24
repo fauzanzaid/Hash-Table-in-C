d84386440b9c1945a86d3a29f44051e331af5a0b
fauzanzaid <fauzanzaid.dev@gmail.com>
2018-01-24 21:37:53 +0530

    Add documentation

--- include/HashTable.h
+++ include/HashTable.h
@@ -6,6 +6,9 @@
 // Data Structures //
 /////////////////////
 
+/**
+ * Opaque Hashtabe struct
+ */
 typedef struct HashTable HashTable;
 
 
@@ -13,18 +16,42 @@ typedef struct HashTable HashTable;
 // Constructors and Destructors //
 //////////////////////////////////
 
+/**
+ * Allocates space for and initializes a Hashtable struct and returns a pointer
+ * to it
+ * @param  size          Fixed size for the hash table
+ * @param  hash_function Pointer to a hash function that returns the hash for
+ * the key whose pointer is passed as an argument
+ * @return 
+ */
 HashTable *HashTable_new(int size, int (*hash_function)(void *));
 
-void *HashTable_destroy(HashTable *htable);
+/**
+ * Deallocate all internally allocated memory
+ * @param  htable_ptr Pointer to a HashTable struct
+ */
+void *HashTable_destroy(HashTable *htable_ptr);
 
 
 ////////////////
 // Manipulate //
 ////////////////
 
-void HashTable_add(HashTable *htable, void *key, void *value);
+/**
+ * Adds element into the hashtable
+ * @param htable_ptr Pointer to HashTable struct
+ * @param key        Pointer to the key which will be used for indexing
+ * @param value      Pointer which will be stored at the key's index
+ */
+void HashTable_add(HashTable *htable_ptr, void *key, void *value);
 
-void *HashTable_find(HashTable *htable, void *key);
+/**
+ * Searches the hashtable for a value indexed by key and returns the value
+ * @param  htable_ptr Pointer to a HashTable struct
+ * @param  key        Pointer to key which will be used to search
+ * @return            Pointer to value if found, otherwise NULL
+ */
+void *HashTable_find(HashTable *htable_ptr, void *key);
 
 
 #endif
